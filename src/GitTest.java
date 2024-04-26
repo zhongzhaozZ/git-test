7caf58132821c34ea5fe4a65a3cf1ed995285781
zhongzhao <zhong_zhaowork@163.com>
2024-04-26 08:05:54 +0800

    push commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("git test1");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
